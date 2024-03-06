f84893b7af5d725560f1833d326d968fed166a23
GypsyBoy <fryy1144fyr@gmail.com>
2024-03-06 16:27:43 +0330

    commit

--- crawler.py
+++ crawler.py
@@ -5,14 +5,16 @@ import netaddr
 def scan_IP(Start_IP: str, End_IP: str, Attack_function=None):
 	ip_range = netaddr.IPRange(Start_IP, End_IP)
 	for ip in ip_range:
-		port_num = 1
-		for i in range(65555):
+		for port in range(65535):
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			try:
-				sock.connect((ip,port_num))
-				print(f"[+] Open: {ip}:{port_num}")
-			except:
-				print(f"[-] Closed: {ip}:{port_num}")
-			port_num += 1
+			sock.settimeour(1)
+			result = sock.connect_ez((str(ip),port))
+			res = (result==0)
+			if res == True:
+				print(f"[+] Open: {ip}:{port}")
+			else:
+				print(f"[-] Close: {ip}:{port}")
+			sock.close()
 
-scan_IP("10.10.10.10", "10.10.10.12")
+
+scan_IP("192.168.1.105","192.168.1.106")
